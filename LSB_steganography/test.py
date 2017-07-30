b3abce39e3eca0fb2f52f2b9f84532c71914b049
VasilisG <vasilisgoutz08@gmail.com>
2017-07-30 11:47:16 +0300

    Add files via upload

---
+++ LSB_steganography/test.py
@@ -0,0 +1,18 @@
+import os
+import lsbSteg
+
+message = "This is a test message to be embedded in an image."
+imageName = "stars_background.jpg"
+stegoImageName = "stego_stars_background.png"
+
+imageWorkingDirectory = os.getcwd()
+imageWorkingDirectory = workingDirectory + "\\" + imageName
+
+stegoImageWorkingDirectory = os.getcwd()
+stegoImageWorkingDirectory = stehoImageWorkingDirectory + "\\" + stegoImageName
+
+lsbSteg.embedMessage(imageWorkingDirectory, stegoImageWorkingDirectory, message)
+
+stegoMessage = lsbSteg.extractMessage(stegoImageWorkingDirectory)
+
+print(stegoMessage)
