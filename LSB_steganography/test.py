1c2eeb654cfdbdf07954a5ab37593dcb29ff130b
Vasilis G <vasilisgoutz08@gmail.com>
2018-08-26 16:20:40 +0300

    Update test.py

--- LSB_steganography/test.py
+++ LSB_steganography/test.py
@@ -5,10 +5,10 @@ message = "This is a hidden text in an image"
 imageFilename = "stars_background.jpg"
 newImageFilename = "stego_stars_background"
 
-newImg = encodeLSB(message, imageFilename, newImageFilename)
+newImg = LsbSteg.encodeLSB(message, imageFilename, newImageFilename)
 if not newImg is None:
         print("Stego image created.")
 
 print("Decoding...")
-message = decodeLSB("stego_stars_background.png")
+message = LsbSteg.decodeLSB("stego_stars_background.png")
 print("Final message: ", message)
